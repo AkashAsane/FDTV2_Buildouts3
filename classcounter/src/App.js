838f9c24c5522e7f7ace1a84fece4623523a489a
AkashAsane <akashasane4455@gmail.com>
2024-05-11 12:03:19 +0530

    test commit

--- classcounter/src/App.js
+++ classcounter/src/App.js
@@ -1,26 +1,39 @@
-import React, { useEffect, useState } from 'react';
 import './App.css';
+import React from 'react';
 
-function Counter() {
-    const[count,setCount]=useState(0)
+class Counter extends React.Component {
+    
+      constructor()
+      {
+        super()
+        this.state={
+            count:0,
+        }
+      }
+             
+      increment(){
+         this.setState((prevstate)=>({count:prevstate.count+1}))
+      }
 
-    let increment=()=>{
-        setCount(count+1);
-    }  
+      decrement(){
+        this.setState((prevstate)=>({count:prevstate.count-1}))
+     }
 
-    let decrement=()=>{
-        setCount(count-1);
-    }  
 
-    return(
-       <div>
-           <h1>Counter App</h1>
-           <div>Count: {count}</div>
-           <button type='button' onClick={increment}>Increment</button>
-           <button type='button' onClick={decrement}>Decrement</button>
-
-       </div>
-    )
+      render()
+      {     return(
+           <div>
+               <div>Counter App</div>
+               <p>Count: {this.state.count}</p>
+               <div>
+               <button onClick={()=>this.increment()}>Increment</button>
+               <button onClick={()=>this.decrement()}>decrement</button>
+               </div>
+           </div>
+         )
+      }
+    
+    
 }
 
 export default Counter;
